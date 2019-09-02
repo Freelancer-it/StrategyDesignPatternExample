fd8d2b6c6f0a03b41de9814bb728a937ae20cc59
gameplay <paulvisser6@hotmail.com>
2019-09-02 16:39:20 +0200

    Initial check-in of module StrategyDesignPatternExample

---
+++ StrategyDesignPatternExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StrategyDesignPatternExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
